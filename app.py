9a4803ad8d5c02889b95cb467a5142ae649acab4
Ranjana Dewangan <ranjanadewangan04@gmail.com>
2025-09-25 17:27:46 +0530

    Update app.py to use Streamlit secrets and ignore secrets file

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import pickle
 import pandas as pd
 import requests
 
+api_key = st.secrets["tmdb"]["key"]
+
 # ---------------- Functions ----------------
 def fetch_poster(movie_id):
     try:
         response = requests.get(
-            f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+            f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
         )
         data = response.json()
         return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
